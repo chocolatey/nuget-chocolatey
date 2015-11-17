461edf035708361ca71636f9c257ccec2bb91c92
eugene.tolmachev <eugene.tolmachev@prolucid.ca>
2015-11-17 15:42:53 -0500

    (chocolatey/choco/gh-399) Client certificate provider
    Added static provider property to resolve client certificates to append to http requests.

---
+++ src/Core/Http/IClientCertificateProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace NuGet
+{
+    /// <summary>
+    /// This interface represents the basic interface that one needs to implement in order to
+    /// support client certificate authentication. 
+    /// </summary>
+    public interface IClientCertificateProvider
+    {
+        /// <summary>
+        /// </summary>
+        X509Certificate GetCertificate(Uri uri);
+    }
+}
---
+++ src/Core/Http/NullClientCertificateProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace NuGet
+{
+    public class NullClientCertificateProvider : IClientCertificateProvider
+    {
+        private static readonly NullClientCertificateProvider _instance = new NullClientCertificateProvider();
+
+        public static IClientCertificateProvider Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        private NullClientCertificateProvider()
+        {
+
+        }
+
+        public X509Certificate GetCertificate(Uri uri)
+        {
+            return null;
+        }
+    }
+}
